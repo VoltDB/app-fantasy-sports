3edc41ae9cdf23a81750e0ed5c16e377b5b51fc2
John Hugg <jhugg@voltdb.com>
2016-04-06 13:15:31 -0400

    WIP refresh to be inline with new examples. Does not work yet.

--- client/fantasysports/Ranker.java
+++ client/fantasysports/Ranker.java
@@ -1,4 +1,27 @@
-package benchmark;
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2016 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package fantasysports;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -25,12 +48,12 @@ public class Ranker implements Runnable {
         try {
 
             ArrayList<SyncCallback> callbacks = new ArrayList<SyncCallback>();
-            
+
             for (int partVal : partitionKeys) {
 
                 SyncCallback cb = new SyncCallback();
                 callbacks.add(cb);
-            
+
                 client.callProcedure(cb,
                                      "SelectContestScoresInPartition",
                                      partVal,
@@ -38,9 +61,9 @@ public class Ranker implements Runnable {
 
             }
 
-            // keep a queue of the VoltTables 
+            // keep a queue of the VoltTables
             PriorityQueue<ScoreTable> pq = new PriorityQueue<ScoreTable>(partitionKeys.size());
-            
+
             for (SyncCallback cb : callbacks) {
                 cb.waitForResponse();
                 VoltTable t = cb.getResponse().getResults()[0];
@@ -57,7 +80,7 @@ public class Ranker implements Runnable {
 
                 // get the list with the highest top score
                 ScoreTable st = pq.remove();
-                
+
                 // increment rank if not the same as the last score
                 count++;
                 //rank++;
@@ -68,12 +91,12 @@ public class Ranker implements Runnable {
 
                 // output the record
                 upsertScore(st, rank);
-                
+
                 // advance table to the next score.  If there was one, add table back to the Priority Queue
                 if (st.advance()) {
                     pq.add(st);
                 }
-                
+
             }
 
             //} catch (IOException | ProcCallException e) {
@@ -82,8 +105,8 @@ public class Ranker implements Runnable {
             //failureCount.incrementAndGet();
             e.printStackTrace();
         }
-    }   
-    
+    }
+
 
     private void upsertScore(ScoreTable t, int rank) throws IOException {
 
@@ -94,9 +117,9 @@ public class Ranker implements Runnable {
                              t.score,
                              rank
                              );
-        
+
     }
-    
+
 
 
     // sortable wrapper of a VoltTable of scores for one partition
@@ -120,7 +143,7 @@ public class Ranker implements Runnable {
         public int getUserId() {
             return (int)t.getLong(0);
         }
-        
+
         public boolean advance() {
             boolean hasNext = t.advanceRow();
             if (hasNext) {
@@ -129,6 +152,6 @@ public class Ranker implements Runnable {
             return hasNext;
         }
     }
-    
+
 
 }
